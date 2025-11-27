5c843318421ddc61d17a58dfd885c5431ab8f626
pranaysj <pranaysj3@gmail.com>
2025-11-27 19:14:52 +0530

    Add Z boundary
    Added a z boundary when it goes out of the z direction, then it will get destory.

--- Subway Surfer/Assets/Scripts/Environment/Coins/Coins.cs
+++ Subway Surfer/Assets/Scripts/Environment/Coins/Coins.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Coins : MonoBehaviour
 {
     public float laneIndex;
+    private int zOutOfBound = -5;
 
     private void Start()
     {
@@ -14,6 +15,11 @@ public class Coins : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * 3);
+
+        if (transform.position.z < zOutOfBound)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
--- Subway Surfer/Assets/Scripts/Environment/Obstacles/Ramp.cs
+++ Subway Surfer/Assets/Scripts/Environment/Obstacles/Ramp.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Ramp : MonoBehaviour
 {
+    private int zOutOfBound = -5;
+
     void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * 3);
+
+        if (transform.position.z < zOutOfBound)
+        {
+            Destroy(gameObject);
+        }
     }
    
 }
